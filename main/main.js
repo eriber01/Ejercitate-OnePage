3cf7515f31f50ee4b1b14a303f700b6a4dcf4852
eriber01 <59887012+eriber01@users.noreply.github.com>
2020-09-02 19:32:16 -0400

    pagina al 100%
    la pagina esta lista si da tiempo el extra sera agregado

---
+++ main/main.js
@@ -0,0 +1,42 @@
+//recargar la pagina al hacer click en inicio
+
+const reload = document.getElementById('reload')
+reload.addEventListener('click', (eve)=>{
+    eve.preventDefault()
+    location.reload()
+})
+
+//controla el boton de ir hacia arriba
+
+    //analiza el scroll para hacer visible el boton
+window.onscroll = function(){
+    console.log(document.documentElement.scrollTop)
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('#scroll-up').classList.add('display-up');
+    }else{
+        document.querySelector('#scroll-up').classList.remove('display-up');
+    }
+}
+
+//boton arriba
+Subir_o_Bajar('#scroll-up', 0)
+
+//boton ir a servisios
+Subir_o_Bajar('#servicio-link', 625)
+
+//boton ir a nosotros
+Subir_o_Bajar('#nosotros-link', 2616)
+
+//boton ir a contactos
+Subir_o_Bajar("#contact-link", 9999999999999999999)
+
+
+//logica de subir o bajar
+function Subir_o_Bajar(referecia, distacia){
+    document.querySelector(referecia).addEventListener('click', function(){
+        window.scrollTo({
+            top: distacia,
+            behavior: 'smooth'
+        })
+    })
+}
